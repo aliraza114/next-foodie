011b7b33c333104315b589e3fd0209f94c7d3c4b
aliraza114 <aliraza3569@gmail.com>
2024-06-03 13:51:20 +0500

    add detail page

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFount() {
+    return <main className="not-found">
+        <h1>Not Found</h1>
+        <p>Sorry, we could not found the requested resource.</p>
+    </main>
+}
